3624dd18441890d8dd172dc6f2bd20b3f9037511
chaofengc <chaofenghust@gmail.com>
2022-06-20 13:43:27 +0800

    refactor: improve model inference

--- inference_femasr.py
+++ inference_femasr.py
@@ -4,10 +4,8 @@ import glob
 import os
 from tqdm import tqdm
 import torch
-import torch.nn.functional as F
 
 from basicsr.utils import img2tensor, tensor2img, imwrite 
-from basicsr.data.transforms import mod_crop
 from basicsr.archs.femasr_arch import FeMaSRNet 
 
 
@@ -20,15 +18,15 @@ def main():
     parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
     parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
     parser.add_argument('--suffix', type=str, default='', help='Suffix of the restored image')
-    parser.add_argument('--mod_scale', type=int, default=16, help='Pre padding size to be divisible by 16')
+    parser.add_argument('--max_size', type=int, default=600, help='Max image size for whole image inference, otherwise use tiled_test')
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
     
     # set up the model
-    qsr_model = FeMaSRNet(codebook_params=[[32, 1024, 512]], LQ_stage=True).to(device)
-    qsr_model.load_state_dict(torch.load(args.weight)['params'], strict=False)
-    qsr_model.eval()
+    sr_model = FeMaSRNet(codebook_params=[[32, 1024, 512]], LQ_stage=True, scale_factor=args.out_scale).to(device)
+    sr_model.load_state_dict(torch.load(args.weight)['params'], strict=False)
+    sr_model.eval()
     
     os.makedirs(args.output, exist_ok=True)
     if os.path.isfile(args.input):
@@ -45,12 +43,12 @@ def main():
         img_tensor = img2tensor(img).to(device) / 255.
         img_tensor = img_tensor.unsqueeze(0)
 
-        max_size = 600 * 600
+        max_size = args.max_size ** 2 
         h, w = img_tensor.shape[2:]
         if h * w < max_size: 
-            output = qsr_model.test(img_tensor)
+            output = sr_model.test(img_tensor)
         else:
-            output = qsr_model.test_tile(img_tensor)
+            output = sr_model.test_tile(img_tensor)
         output_img = tensor2img(output)
 
         save_path = os.path.join(args.output, f'{img_name}')
